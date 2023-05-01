485b2c0b088c5d89b5c4f1a8e1ec9c63dd2e860d
Luewngier <g217881111@gmail.com>
2023-05-01 23:33:45 +0800

    feat: update getListings action

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,8 +1,75 @@
 import prisma from '@/libs/prismadb'
 
-export default async function getListings() {
+export interface IListingsParams {
+  userId?: string
+  guestCount?: number
+  roomCount?: number
+  bathroomCount?: number
+  startDate?: string
+  endDate?: string
+  locationValue?: string
+  category?: string
+}
+
+export default async function getListings(params: IListingsParams) {
   try {
+    const {
+      userId,
+      guestCount,
+      roomCount,
+      bathroomCount,
+      startDate,
+      endDate,
+      locationValue,
+      category,
+    } = params
+
+    let query = {
+      ...(userId && { userId }),
+      ...(category && { category }),
+      ...(locationValue && { locationValue }),
+
+      ...(guestCount && {
+        guestCount: {
+          gte: +guestCount,
+        },
+      }),
+
+      ...(roomCount && {
+        roomCount: {
+          gte: +roomCount,
+        },
+      }),
+
+      ...(bathroomCount && {
+        bathroomCount: {
+          gte: +bathroomCount,
+        },
+      }),
+
+      ...(startDate &&
+        endDate && {
+          NOT: {
+            reservations: {
+              some: {
+                OR: [
+                  {
+                    endDate: { gte: startDate },
+                    startDate: { lte: startDate },
+                  },
+                  {
+                    endDate: { gte: endDate },
+                    startDate: { lte: endDate },
+                  },
+                ],
+              },
+            },
+          },
+        }),
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: 'desc',
       },
