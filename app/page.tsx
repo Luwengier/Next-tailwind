136962170065e63542b582cdddba707ef35d9b33
Luewngier <g217881111@gmail.com>
2023-05-07 23:40:22 +0800

    feat: update home listings page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,46 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Container from './components/Container'
+import EmptyState from './components/EmptyState'
+import getCurrentUser from './actions/getCurrentUser'
+import getListings, { IListingsParams } from './actions/getListings'
+import ListingCard from './components/listings/ListingCard'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hi Airbnb clone !</div>
+interface HomeProps {
+  searchParams: IListingsParams
 }
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const listings = await getListings(searchParams)
+  const currentUser = await getCurrentUser()
+
+  if (listings.length === 0) {
+    return <EmptyState showReset />
+  }
+
+  return (
+    <Container>
+      <div
+        className="
+        pt-24
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+      "
+      >
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            currentUser={currentUser}
+            data={listing}
+          />
+        ))}
+      </div>
+    </Container>
+  )
+}
+
+export default Home
